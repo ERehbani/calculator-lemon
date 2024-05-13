f693f423b042432915bc67bda9e5814bb7f46479
Elian Rehbani <elianrehbani@MacBook-Pro.fibertel.com.ar>
2024-05-13 19:37:21 -0300

    lemon calculator

--- src/options/options-data.js
+++ src/options/options-data.js
@@ -1,15 +1,15 @@
 const options = [
-  { value: "BTC", label: "BTC", image: { src: "../../public/btc_calc.svg" } },
+  { value: "BTC", label: "BTC", image: { src: "./btc_calc.svg" } },
   {
     value: "SAT",
     label: "SAT",
-    image: { src: "../../public/satoshi_calc.svg" },
+    image: { src: "./satoshi_calc.svg" },
   },
-  { value: "ARS", label: "ARS", image: { src: "../../public/ars_calc.svg" } },
+  { value: "ARS", label: "ARS", image: { src: "./ars_calc.svg" } },
   {
     value: "USDT",
     label: "USDT",
-    image: { src: "../../public/usdt_calc.svg" },
+    image: { src: "./usdt_calc.svg" },
   },
 ];
 
